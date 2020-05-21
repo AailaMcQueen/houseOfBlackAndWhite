01eacefa02d4834b5c479ceb552eb2d4e027c660
theclassicwizard <srkjadara@gmail.com>
2020-05-21 14:58:54 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,31 +8,31 @@ var url = "https://randomuser.me/api/";
 var rowAnimate = document.getElementsByClassName("row")[0];
 
 fetch(url)
-    .then(function(request){
-        if(!request.ok){
-            throw Error(request.status);
-        }
-        return request.json();
-    })
-    .then(function(response){
-        var data = response.results[0];
-        //rowAnimate.style.webkitAnimationPlayState = "running";
-        rowAnimate.classList.add("myAnimate");
-        nameProf.innerHTML = data.name.title + " " + data.name.first + " " + data.name.last;
-        city.innerHTML = data.location.city;
-        username.innerHTML = data.login.username;
-        email.innerHTML = data.email;
-        image.src = data.picture.large;
-        setTimeout(function(){
-            //rowAnimate.style.webkitAnimationPlayState = "paused";
-            rowAnimate.classList.remove("myAnimate");
-            rowAnimate.style.opacity = 1;
-        }, 900);
-        
-    })
-    .catch(function(error){
-        alert(error);
-    });
+.then(function(request){
+    if(!request.ok){
+        throw Error(request.status);
+    }
+    return request.json();
+})
+.then(function(response){
+    var data = response.results[0];
+    //rowAnimate.style.webkitAnimationPlayState = "running";
+    rowAnimate.classList.add("myAnimate");
+    nameProf.innerHTML = data.name.title + " " + data.name.first + " " + data.name.last;
+    city.innerHTML = data.location.city;
+    username.innerHTML = data.login.username;
+    email.innerHTML = data.email;
+    image.src = data.picture.large;
+    setTimeout(function(){
+        //rowAnimate.style.webkitAnimationPlayState = "paused";
+        rowAnimate.classList.remove("myAnimate");
+        rowAnimate.style.opacity = 1;
+    }, 900);
+    
+})
+.catch(function(error){
+    alert(error);
+});
 
 button.addEventListener("click", function(){
 
