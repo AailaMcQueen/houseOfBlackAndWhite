3c632d7dbb314ba3ac0e948f1907e54beebe00be
theclassicwizard <srkjadara@gmail.com>
2020-05-28 09:52:26 +0530

    firstThingsFirst

--- src/main.js
+++ src/main.js
@@ -13,6 +13,19 @@ var rowAnimate = document.getElementsByClassName("row")[0];
 var x = document.getElementById("myAudio"); 
 var enterButton = document.querySelector(".tik");
 var hof = document.querySelector(".hof");
+var musicOn = document.querySelector(".musicOn");
+var musicOff = document.querySelector(".musicOff");
+
+var houseInfo = "The House of Black and White is a temple in Braavos dedicated to the Many-Faced God. It serves as the headquarters of the guild of religious assassins known as the <strong>Faceless Men</strong>."; 
+var houseInfo2 = "<br>It sits alone on a small island in the lagoon of Braavos. Although it can be reached by boat or bridge from other locations in the city, the island is usually deserted.";
+
+var rules = [
+    "<strong>They kill in inconspicuous and clever ways.</strong> The Faceless Men don't like to call attention to themselves or their murders. They offer what is known as a <strong>precise</strong> killing, with no other casualties, that might look like an accident more often than not. They are believed to most commonly use a poison called <strong>The Strangler</strong>.",
+    "<strong>They serve the Many-Faced God and do not decide who dies.</strong> The Faceless Men see themselves as the servants of their god. They are merely acting in accordance with the will of this higher power.",
+    "<strong>They have left their identities behind.</strong> This is one of the core principles of a Faceless Man. You must give over everything to the Many-Faced God.",
+    "<strong>They are not supposed to know their victims.</strong> The Faceless Men must let go of their personal vendettas, and they're not supposed to have previously met their victims. This seems to align with the principles of any assassin. It is a contractual kill, it is not personal. The murderer must never have an emotional stake in it."
+]
+
 
 function playAudio() { 
   x.play(); 
@@ -99,6 +112,8 @@ button.addEventListener("click", function(){
 
 enterButton.addEventListener("click", function(){
     $(".openingCover").fadeTo(1500, 0, function(){
+        musicOn.style.opacity = 1;
+        musicOff.style.opacity = 0.5;
         $(".openingCover").css("display", "none");
         $(".introductory").fadeTo(1000, 1);
     });  
@@ -118,20 +133,32 @@ $('.expand').click(function() {
         }
       }
     );
-  });
+});
 
 
-  hof.addEventListener("click", function(){
-    $(".introductory").fadeTo(1500, 0, function(){
-        $(".introductory").css("display", "none");
-        $(".container").css("display", "flex");
-        $(".container").fadeTo(1000, 1);
-    }); 
-  })
+hof.addEventListener("click", function(){
+$(".introductory").fadeTo(1500, 0, function(){
+    $(".introductory").css("display", "none");
+    $(".container").css("display", "flex");
+    $(".container").fadeTo(1000, 1);
+}); 
+})
 
-  buttonHome.addEventListener("click", function(){
-    $(".container").fadeTo(1500, 0, function(){
-        $(".container").css("display", "none");
-        $(".introductory").fadeTo(1000, 1);
-    });
-  })
+buttonHome.addEventListener("click", function(){
+$(".container").fadeTo(1500, 0, function(){
+    $(".container").css("display", "none");
+    $(".introductory").fadeTo(1000, 1);
+});
+})
+
+musicOn.addEventListener("click", function(){
+    x.muted = false;
+    musicOn.style.opacity = 1;
+    musicOff.style.opacity = 0.5;
+})
+
+musicOff.addEventListener("click", function(){
+    x.muted = true;
+    musicOn.style.opacity = 0.5;
+    musicOff.style.opacity = 1;
+})
